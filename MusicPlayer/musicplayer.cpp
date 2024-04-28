d163390903b7b5e8523dfa874e9026ebc5521195
mateusz.maslanka <mateusz.maslanka1@outlook.com>
2024-04-28 16:57:18 +0200

    add select song list

--- MusicPlayer/musicplayer.cpp
+++ MusicPlayer/musicplayer.cpp
@@ -13,21 +13,29 @@ void MusicPlayer::findInMusicLibrary() {
     QVector<QString> songList;
     QVector<QString> songListFullPath;
     QSettings settings("PathToUserDirectory");
-    QString musicDirectory = settings.value("path").toString();
+
     char* username = getlogin();
-    std::string pathToDirectory = "/home/" + std::string(username) + "/Music";
-    if (std::filesystem::exists(pathToDirectory) && std::filesystem::is_directory(pathToDirectory)) {
-        for (const auto& entry : std::filesystem::directory_iterator(musicDirectory.toStdString())) {
+    std::string pathToMusicDirectory = "/home/" + std::string(username) + "/Music";
+
+    if (!std::filesystem::exists(pathToMusicDirectory) && !std::filesystem::is_directory(pathToMusicDirectory)) {
+        std::filesystem::create_directory(pathToMusicDirectory);
+        settings.setValue(QString::fromStdString(pathToMusicDirectory), QString::fromStdString(pathToMusicDirectory));
+
+    } else if(!settings.contains(pathToMusicDirectory)) {
+        settings.setValue(QString::fromStdString(pathToMusicDirectory), QString::fromStdString(pathToMusicDirectory));
+    }
+    QStringList allKeys = settings.allKeys();
+    for (const QString &key : allKeys) {
+        QString value = settings.value(key).toString();
+        for (const auto& entry : std::filesystem::directory_iterator(value.toStdString())) {
             songList.append(QString::fromStdString(entry.path().filename()));
             songListFullPath.append(QString::fromStdString(entry.path()));
         }
-        setSomeData(songList);
-        setPath(songListFullPath);
-        musicLibraryChanged(songListFullPath);
-    } else {
-        std::filesystem::create_directory(pathToDirectory);
-        settings.setValue("path", QString::fromStdString(pathToDirectory));
     }
+
+    setSomeData(songList);
+    setPath(songListFullPath);
+    musicLibraryChanged(songListFullPath);
 }
 
 void MusicPlayer::getPathToDirectory(QString pathToDirectory) {
@@ -48,7 +56,7 @@ void MusicPlayer::getPathToDirectory(QString pathToDirectory) {
         }
     }
     if (!valueExists) {
-        settings.setValue("path", cleanedPath);
+        settings.setValue(cleanedPath, cleanedPath);
         qInfo() << "Wartość '" << cleanedPath << "' została pomyślnie zapisana w pliku konfiguracyjnym.";
     }
 }
